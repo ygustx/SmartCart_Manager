0e79daa1d3a91b474285db33a718df56c203131f
ALUNO <ALUNO@senaiescola.fsa>
2023-12-06 10:53:44 -0300

    atualização2

---
+++ CERTO/app/src/main/java/com/example/certo/FormFuncionario.java
@@ -0,0 +1,14 @@
+package com.example.certo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormFuncionario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_funcionario);
+    }
+}
